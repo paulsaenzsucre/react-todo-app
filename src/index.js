651b3ee5b1529a701e4ae156508b2db5c962c12d
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-16 13:41:07 -0500

    Add state to TodoContainer.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TodoContainer from './components/TodoContainer';
 
-const element = <h1>Hello from Create React App</h1>;
-
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
