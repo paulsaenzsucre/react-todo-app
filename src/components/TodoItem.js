633d8108740721e39138879abb7f21c59f3da632
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-17 06:48:20 -0500

    Refactor TodoItem to class component.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,39 +1,41 @@
 import React from 'react';
 import styles from './TodoItem.module.css';
 
-function TodoItem(props) {
-  const completedStyle = {
-    fontStyle: 'italic',
-    color: '#595959',
-    opacity: 0.4,
-    textDecoration: 'line-through',
-  };
-
-  const { completed, id, title } = props.todo;
-
-  const handleEditing = () => {
+class TodoItem extends React.Component {
+  handleEditing = () => {
     console.log('edit mode activated');
   };
 
-  return (
-    <li className={styles.item}>
-      <div onDoubleClick={handleEditing}>
-        <input
-          type="checkbox"
-          className={styles.checkbox}
-          checked={completed}
-          onChange={() => props.handleChangeProps(id)}
-        />
-        <button onClick={() => props.deleteTodoProps(id)}>
-          Delete
-        </button>
-        <span style={completed ? completedStyle : null}>
-          {title}
-        </span>
-      </div>
-      <input type="text" className={styles.textInput} />
-    </li>
-  );
+  render() {
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: '#595959',
+      opacity: 0.4,
+      textDecoration: 'line-through',
+    };
+
+    const { completed, id, title } = this.props.todo;
+
+    return (
+      <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing}>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={completed}
+            onChange={() => this.props.handleChangeProps(id)}
+          />
+          <button onClick={() => this.props.deleteTodoProps(id)}>
+            Delete
+          </button>
+          <span style={completed ? completedStyle : null}>
+            {title}
+          </span>
+        </div>
+        <input type="text" className={styles.textInput} />
+      </li>
+    );
+  }  
 }
 
 export default TodoItem;
