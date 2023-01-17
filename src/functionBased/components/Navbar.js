b12b7de6ed28d200be895abf8b36889a00f5a5ae
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-17 16:53:52 -0500

    Add style to navigation links.

--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
@@ -20,13 +22,28 @@ const Navbar = () => {
     setNavbarOpen(!navbarOpen);
   };
 
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
   return (
     <nav className="navBar">
-      <button type="button" onClick={handleToggle}>{navbarOpen ? 'Close' : 'Open'}</button>
+      <button type="button" onClick={handleToggle}>
+        {navbarOpen ? (
+          <MdClose style={{ color: '#fff', width: '40px', height: '40px' }} />
+        ) : (
+          <FiMenu style={{ color: '#7b7b7b', width: '40px', height: '40px' }} />
+        )}
+      </button>
       <ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
         {links.map((link) => (
           <li key={link.id}>
-            <NavLink to={link.path} className={(navData) => (navData.isActive ? 'active-link' : 'none')} exact="true">
+            <NavLink
+              to={link.path}
+              className={(navData) => (navData.isActive ? 'active-link' : 'none')}
+              onClick={() => closeMenu()}
+              exact="true"
+            >
               {link.text}
             </NavLink>
           </li>
