31641b327b7bce1fba98dd0e934accd224d4c4d1
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-17 05:55:16 -0500

    Add tag completed todo feature.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,18 +2,29 @@ import React from 'react';
 import styles from './TodoItem.module.css';
 
 function TodoItem(props) {
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+
+  const { completed, id, title } = props.todo;
+
   return (
     <li className={styles.item}>
       <input
         type="checkbox"
         className={styles.checkbox}
-        checked={props.todo.completed}
-        onChange={() => props.handleChangeProps(props.todo.id)}
+        checked={completed}
+        onChange={() => props.handleChangeProps(id)}
       />
-      <button onClick={() => props.deleteTodoProps(props.todo.id)}>
+      <button onClick={() => props.deleteTodoProps(id)}>
         Delete
       </button>
-      {props.todo.title}
+      <span style={completed ? completedStyle : null}>
+        {title}
+      </span>
     </li>
   );
 }
